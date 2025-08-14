6ef18c5cc8a2f895c7fc0f9e41accd74a8c786ef
Musharraf2000 <musharraf.azhar19@gmail.com>
2025-08-14 06:36:14 +0530

    fix 2

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
-self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open("v1").then(cache => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/images/manifest.json",
-          "/images/android-icon-192x192.png" // Add your main icon here
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+          '/Gugu/',  // Update this path
+          '/Gugu/index.html',  // Update this path
+          '/Gugu/styles/styles.css',  // Update this path
+          '/Gugu/scripts/script.js',  // Update this path
+          '/Gugu/favicons/favicon-192x192.png',  // Update this path
+          '/Gugu/favicons/favicon-512x512.png'  // Update this path
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
